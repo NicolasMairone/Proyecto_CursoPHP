8c0c677808a8ad37dfe95687507b34284480f1da
NicolasMairone <71902719+NicolasMairone@users.noreply.github.com>
2021-07-28 17:05:50 -0300

    Update validarlog.php

--- validarlog.php
+++ validarlog.php
@@ -1,7 +1,7 @@
 <?php
-    $usuario=$_POST['usuario'];
-	$email=$_POST['email'];
-	$clave=$_POST['clave'];
+    $usuario=$_GET['usuario'];
+	$email=$_GET['email'];
+	$clave=$_GET['clave'];
 	//conecto con la BDD
 	include 'conexion.php';
 	$consulta="Select * from usuarios where nom_usu='$usuario' and ema_usu='$email' and pas_usu='$clave'";
